98ba5c9188a5c2b46999d097cb7613319d4d16a8
Michael Mauderer <mail@michaelmauderer.com>
2017-07-16 18:23:44 +0200

    Move edge reversal test from polyhedron.go to edge_test.go.

--- polyhedra/edges_test.go
+++ polyhedra/edges_test.go
@@ -1,1 +1,18 @@
 package polyhedra
+
+import "testing"
+
+func TestEdgeReversal(t *testing.T) {
+	e1 := Edge{NewVertex(), NewVertex()}
+	e2 := Edge{e1.v2, e1.v1}
+
+	if e1.Reversed() != e2 {
+		t.Error("Edge.Reversed does not proiduce the reversed Edge.")
+	}
+	if e1.Reversed() == e1 {
+		t.Error("edge.Reversed equals itself.")
+	}
+	if e1.Reversed().Reversed() != e1 {
+		t.Error("Twice reversed edge not euqal itself.")
+	}
+}
--- polyhedra/polyhedron_test.go
+++ polyhedra/polyhedron_test.go
@@ -7,21 +7,6 @@ import (
 	"github.com/MichaelMauderer/polyhedra/r3"
 )
 
-func TestEdgeReversal(t *testing.T) {
-	e1 := Edge{NewVertex(), NewVertex()}
-	e2 := Edge{e1.v2, e1.v1}
-
-	if e1.Reversed() != e2 {
-		t.Error("Edge.Reversed does not proiduce the reversed Edge.")
-	}
-	if e1.Reversed() == e1 {
-		t.Error("edge.Reversed equals itself.")
-	}
-	if e1.Reversed().Reversed() != e1 {
-		t.Error("Twice reversed edge not euqal itself.")
-	}
-}
-
 func shuffledVertices(a []Vertex) []Vertex {
 	shuffled := make([]Vertex, len(a))
 	perm := rand.Perm(len(a))
